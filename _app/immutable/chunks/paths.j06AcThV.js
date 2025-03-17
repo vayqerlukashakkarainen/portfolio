37361b3e49dbb9bbd301902ccb9d256ca5b2af4e
Lukas Hakkarainen <lukas@vayqer.se>
2025-03-17 15:08:27 +0100

    Updates

---
+++ _app/immutable/chunks/paths.j06AcThV.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_pf9a9j)==null?void 0:s.base)??"/portfolio";var a;const e=((a=globalThis.__sveltekit_pf9a9j)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.srpcBJx4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.AoD7KAJ4.js";export{t as start};
