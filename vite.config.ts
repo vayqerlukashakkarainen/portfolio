b8b2ab49badd3fd16fac8cd35386d45adbd629cf
Lukas Hakkarainen <lukas@vayqer.se>
2025-03-17 13:47:56 +0100

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default async () => {
+	const dev = process.argv.includes('dev');
+
+	var ghpages = await import('gh-pages');
+
+	ghpages.publish('static', { dotfiles: true, add: true });
+
+	return defineConfig({
+		plugins: [sveltekit()],
+		base: dev ? '' : '/portfolio-site'
+	});
+};
