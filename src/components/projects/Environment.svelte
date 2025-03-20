d02681a6ceb58a18e5ad6087a1820cc207ac98c8
Lukas Hakkarainen <lukas@vayqer.se>
2025-03-20 19:28:11 +0100

    Update texts

--- src/components/projects/Environment.svelte
+++ src/components/projects/Environment.svelte
@@ -1,20 +1,23 @@
 <h4>About</h4>
 <p>
-	Planned from the start to become a settlement building game (I have a soft spot for these kind of
-	games), I needed a dynamic environment that could be randomized every game. There are plenty of
-	finished terrain generators in the Unity asset store, but since I wanted to support more than
-	terrain and biomes (and to challenge myself) I developed my own.
+	From the start, I envisioned this as a settlement-building game (I have a soft spot for these). To
+	keep things fresh, I needed a dynamic, randomized environment for every playthrough. While plenty
+	of terrain generators exist in the Unity Asset Store, I wanted something more flexible—one that
+	could handle not just terrain and biomes but additional features as well. Plus, I liked the
+	challenge, so I built my own.
 </p>
 <p>
-	In its current state it supports randomized islands, biomes, nature parameters (fertility,
-	altitude preferences etc), randomized placement for natural props (tree types, rocks, plants,
-	grass etc).
+	In its current state, the system supports randomized islands, biomes, and various natural
+	parameters like fertility and altitude preferences. It also handles randomized placement for
+	natural props, including different tree types, rocks, plants, and grass.
 </p>
 <h4>Challenges</h4>
 <p>
-	Developing an atlas generator was tougher than anticipated, but being naive in any type of
-	development is mostly a good thing in my experience! I think the hardest was to debug when
-	something went wrong, it could either be my code or my input that is causing a bug.
+	Developing an atlas generator turned out to be tougher than I expected—but in my experience, a
+	little naivety in development is usually a good thing! The hardest part was debugging; whenever
+	something went wrong, it was a challenge to figure out whether the issue was in my code or my
+	input. Tho wrapping it all inside a game loop was the hardest part. Which was a reason why the
+	project got stale.
 </p>
 <h4>What I learned</h4>
 <p>
